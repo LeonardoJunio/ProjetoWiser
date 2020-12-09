f213fb45e201f0c69fa3ca9f78d59069bc0ebb15
Leonardo Junio <leonardo.lj.queiroz@gmail.com>
2020-12-08 21:54:22 -0300

    Update placeholder

--- views/usuarios/alterar-senha.php
+++ views/usuarios/alterar-senha.php
@@ -13,9 +13,9 @@ $this->title = 'Alterar Senha';
     <div class="senha-update-body">
         <?php $form = ActiveForm::begin(); ?>
 
-        <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Senha', 'required' => true])->label(false) ?>
+        <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Nova senha', 'required' => true])->label(false) ?>
 
-        <?= $form->field($model, 'passwordConfirm')->passwordInput(['placeholder' => 'Confirmar Senha', 'required' => true])->label(false) ?>
+        <?= $form->field($model, 'passwordConfirm')->passwordInput(['placeholder' => 'Confirmar nova senha', 'required' => true])->label(false) ?>
 
         <div class="text-right">
             <?= Html::submitButton('Salvar Senha', ['class'=>'btn btn-success', 'name' => 'buttonAlterarSenha', 'id' => 'buttonAlterarSenha']); ?>
